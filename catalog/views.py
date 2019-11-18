1d74a34c11c39244a77b98c7020d1b1dbd1ca0e2
alsgil13 <als.gil13@gmail.com>
2019-11-18 16:31:10 -0300

    adicionando páginas

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
 
 # Create your views here.
+from catalog.models import Book, Author, BookInstance, Genre, Language
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    num_books_puzur = Book.objects.filter(title__icontains='puzur').count()
+
+    # The 'all()' is implied by default.    
+    num_authors = Author.objects.count()
+
+# Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_puzur':num_books_puzur,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    #Comentar futuramente
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='espadachin')[:5] # Get 5 books containing the title espadachin
+
+class BookDetailView(generic.DetailView):
+    model = Book    
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    #Comentar futuramente
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='espadachin')[:5] # Get 5 books containing the title espadachin
+
+class AuthorDetailView(generic.DetailView):
+    model = Author    
